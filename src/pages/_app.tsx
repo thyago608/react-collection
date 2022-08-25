af08d4e70a858b9ac6dd26b163f9b5538a077891
thyago608 <thyagoribeiro608@gmail.com>
2022-08-25 09:08:48 -0400

    Added React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,25 @@
 import type { AppProps } from 'next/app';
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
 import { NewProductModal } from 'components/NewProductModal';
 import { ModalProvider } from "contexts/ModalContext";
 import Modal from "react-modal";
 import "styles/global.scss";
+import { queryClient } from 'services/queryClient';
 
 Modal.setAppElement("#__next");
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ModalProvider>
-            <Header />
-            <Component {...pageProps} />
-            <Footer />
-            <NewProductModal />
-        </ModalProvider>
+        <QueryClientProvider client={queryClient}>
+            <ModalProvider>
+                <Header />
+                <Component {...pageProps} />
+                <Footer />
+                <NewProductModal />
+            </ModalProvider>
+        </QueryClientProvider>
     );
 }
 
