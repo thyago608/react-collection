903fefac2fdeb0b0c5b0419cfeef7c470bbe2ee1
thyago608 <thyagoribeiro608@gmail.com>
2022-08-28 13:19:36 -0400

    Updated ToastConfig

--- src/utils/toasts.ts
+++ src/utils/toasts.ts
@@ -1,49 +1,20 @@
-import { toast } from "react-toastify";
+import { toast, ToastOptions } from "react-toastify";
 
-export const toastError = (message: string) =>
-  toast.error(message, {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+const config: ToastOptions = {
+  position: "top-right",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "colored",
+};
 
-export const toastSuccess = (message: string) =>
-  toast.success(message, {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+export const toastError = (message: string) => toast.error(message, config);
 
-export const toastInfo = (message: string) =>
-  toast.info(message, {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+export const toastSuccess = (message: string) => toast.success(message, config);
 
-export const toastWarning = (message: string) =>
-  toast.warning(message, {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+export const toastInfo = (message: string) => toast.info(message, config);
+
+export const toastWarning = (message: string) => toast.warning(message, config);
